6672d6406441520698ab56f7a07cc63151365672
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2011-12-18 23:04:59 +0100

    Fix grid

--- war/js/kickjs_editor.js
+++ war/js/kickjs_editor.js
@@ -10,6 +10,90 @@ YUI().use('tabview', 'escape', 'plugin', function(Y) {
     window.engine = sceneEditorApp.engine;
 });
 
+var VisualGrid = function(){
+    var engine,
+        gl,
+        gridShader,
+        gridMesh,
+        shaderMaterial,
+        transform,
+        thisObj = this;
+
+    this.enabled = true;
+
+    this.activated = function(){
+        engine = this.gameObject.engine;
+        transform = this.gameObject.transform;
+        gl = engine.gl;
+        gridShader = engine.resourceManager.getShader("kickjs://shader/unlit/");
+        shaderMaterial = new KICK.material.Material(engine,{
+            name:"Grid",
+            shader: gridShader,
+            uniforms:{
+                mainColor: {
+                    value: [.2,.2,.2],
+                    type: KICK.core.Constants.GL_FLOAT_VEC3
+                },
+                mainTexture: {
+                    value: engine.resourceManager.getTexture("kickjs://texture/white/"),
+                    type: KICK.core.Constants.GL_SAMPLER_2D
+                }
+            }
+        });
+        var gridMeshData = new KICK.mesh.MeshData();
+        var lines = [];
+        var index = [];
+        var uvs = [];
+        var size = 100;
+        for (var i=-size;i<=size;i++){
+            // one direction - from
+            lines.push(-size);
+            lines.push(0);
+            lines.push(i);
+            // one direction - to
+            lines.push(size);
+            lines.push(0);
+            lines.push(i);
+            // one direction - from
+            lines.push(i);
+            lines.push(0);
+            lines.push(-size);
+            // one direction - to
+            lines.push(i);
+            lines.push(0);
+            lines.push(size);
+            // update indices
+            index.push(index.length);
+            index.push(index.length);
+            index.push(index.length);
+            index.push(index.length);
+            for (var x=0;x<8;x++){
+                uvs.push(0);
+            }
+        }
+        gridMeshData.meshType = KICK.core.Constants.GL_LINES;
+        gridMeshData.vertex = lines;
+        gridMeshData.indices = index;
+        gridMeshData.uv1 = uvs;
+
+
+        gridMesh = new KICK.mesh.Mesh(engine,{
+            name:"GridLines",
+            meshData:gridMeshData
+        });
+
+        var errors = gridMesh.verify(gridShader);
+    };
+    this.render = function(engineUniforms,overwriteShader){
+        if (thisObj.enabled){
+            var shader = overwriteShader || gridShader;
+            gridMesh.bind(shader);
+            shader.bindUniform(shaderMaterial,engineUniforms,transform);
+            gridMesh.render(0);
+        }
+    };
+};
+
 var CameraNavigator = function(){
     var transform,
         mouseInput,
@@ -25,12 +109,22 @@ var CameraNavigator = function(){
             var mousePosDelta = mouseInput.deltaMovement;
             var hasMovement = mousePosDelta[0] || mousePosDelta[1];
             if (hasMovement){
-                console.log("Rotation!!!! ",KICK.math.vec3.str(transform.rotationEuler));
                 var mouseSensitivity = 0.1;
                 euler[0] += mousePosDelta[1]*mouseSensitivity;
                 euler[1] += mousePosDelta[0]*mouseSensitivity;
                 transform.localRotationEuler = euler;
-                console.log("RotationPost ",KICK.math.vec3.str(transform.rotationEuler));
+            }
+        } else if (mouseInput.isButton(1)){
+            var mousePosDelta = mouseInput.deltaMovement;
+            var hasMovement = mousePosDelta[0] || mousePosDelta[1];
+            if (hasMovement){
+                var mousePanSensitivity = 0.01;
+                var movement = [-mousePanSensitivity*mousePosDelta[0],mousePanSensitivity*mousePosDelta[1],0];
+                var rotation = transform.rotation;
+                movement = KICK.math.quat4.multiplyVec3(rotation,movement);
+                var pos = transform.position;
+                pos = KICK.math.vec3.add(pos,movement);
+                transform.position = pos;
             }
         }
         var wheelDelta = mouseInput.deltaWheel;
@@ -40,11 +134,8 @@ var CameraNavigator = function(){
             var rotation = transform.rotation;
             forward = KICK.math.quat4.multiplyVec3(rotation,forward);
             var pos = transform.position;
-            console.log("Po_", KICK.math.vec3.str(pos),"Fwd",KICK.math.vec3.str(forward),"Rot",KICK.math.vec3.str(transform.rotationEuler));
             pos = KICK.math.vec3.add(pos,forward);
-            console.log("Pos", KICK.math.vec3.str(pos),"Fwd",KICK.math.vec3.str(forward),"Rot",KICK.math.vec3.str(transform.rotationEuler));
             transform.position = pos;
-            console.log("PoZ", KICK.math.vec3.str(pos),"Fwd",KICK.math.vec3.str(forward),"Rot",KICK.math.vec3.str(transform.rotationEuler));
         }
     };
 };
@@ -56,6 +147,7 @@ var SceneEditorApp = function(){
         }),
         editorSceneCameraObject,
         editorSceneCameraComponent,
+        editorSceneGridObject,
 
         decorateScene = function(scene){
             editorSceneCameraObject = scene.createGameObject();
@@ -66,6 +158,10 @@ var SceneEditorApp = function(){
             });
             editorSceneCameraObject.addComponent(editorSceneCameraComponent);
             editorSceneCameraObject.addComponent(new CameraNavigator());
+
+            editorSceneGridObject = scene.createGameObject();
+            editorSceneGridObject.name = "__editorSceneGridObject__";
+            editorSceneGridObject.addComponent(new VisualGrid());
 
             // create material
             var material = new KICK.material.Material(engine,{
@@ -151,9 +247,9 @@ function createSceneAssets(Y,engine){
 
         activeScene = engine.activeScene;
         for (i=activeScene.numberOfComponents - 1;i>=0;i--){
-
+//            var gameObject = activeScene.
         }
-    }
+    };
     this.updateSceneContent();
 }
 
